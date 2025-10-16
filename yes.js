9bdb017d406d114d4f36fb0111c752b76c6fbe7e
Jon Maestas <14333179+jonmaestas@users.noreply.github.com>
2025-10-16 10:17:11 -0500

    tsm stuff init

---
+++ yes.js
@@ -0,0 +1,9 @@
+window.location = "mailto:mayor@wrightcity.org"
+  + "?cc=play@wrightcitysports.org"
+  + "&subject=" + encodeURIComponent("Support for TSM Running Wright City Sports Programs")
+  + "&body=" + encodeURIComponent(
+    "Dear Mayor Heiliger and Board of Aldermen,\n\n" +
+    "Yes, I support Tournament Sports Management (TSM) running our sports programs. " +
+    "I believe this will benefit the community and keep youth sports strong in Wright City.\n\n" +
+    "Thank you,\n[Your Name]"
+  );
